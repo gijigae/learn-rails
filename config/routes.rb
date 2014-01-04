740e35c2334a1d8d8c8523410541e01d57bc5ccd
gijigae <gijigae@gmail.com>
2014-01-04 22:42:24 +0900

    Add about.html and update routes.rb to make about.html as a landing page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ LearnRails::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root to: redirect('/about.html')
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
